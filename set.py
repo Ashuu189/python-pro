c0453ac28efe24d787e3aa6c15c780edd966d1c8
Ashutosh <ashut.sarkar8187@gmail.com>
2026-06-28 23:08:28 +0530

    learned basics of Single value data type and multi valued data type like string, list and tuple

---
+++ set.py
@@ -0,0 +1,36 @@
+# set 
+
+# initializing a set
+set1 = {1, 2, 3, 4, 5}
+# print(set1)
+
+# set2={1,0.1,(0+1j),True,"Ashut"}
+# print(set2)
+
+# adding element in set
+set1.add(6)
+# print(set1)
+
+# adding multiple elements in set
+set1.update([7, 8, 9])
+# print(set1)
+
+# removing element from set
+set1.remove(9)
+# print(set1)
+
+# discarding element from set
+set1.discard(8)
+# print(set1)
+
+# popping element from set
+popped_value = set1.pop()
+# print(popped_value)
+# print(set1)
+
+# set1.clear()
+# print(set1)
+
+# making empty set
+set2 = set()
+print(set2)
---
+++ tuple.py
@@ -0,0 +1,15 @@
+# tuple datatype
+
+# way to add single value in tuple
+# tup=(1,)
+# print(tup)
+# print(type(tup))
+
+tup=(1,2,3,4,5,6,7,8,9)
+
+# indexing in tuple
+print(tup[0])
+
+# slicing in tuple
+print(tup[0:4])
+
