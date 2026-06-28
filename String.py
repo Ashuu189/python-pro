8a092564e7cd2b359e617669e24e982ddf2c9538
Ashutosh <ashut.sarkar8187@gmail.com>
2026-06-28 17:37:05 +0530

    started python learning

---
+++ String.py
@@ -0,0 +1,63 @@
+# s="I am ironman"
+
+# print(s[3])
+# print(s[-1])
+
+#  print(s[-4 : -8 :-1]) #nori
+# print(s[0: 6: 3]) #imi
+
+# print(s[0] + s[3] + s[5])
+
+# s="Easy is boring"
+# print(s[8: 15: 1])
+
+# print(s[-6:])
+
+# print(s[5:8:1])
+# print(s[-9: -7: 1 ])
+
+# print(s[0:4])
+
+# print(s[1:4 ])
+
+# print(s[-14:-3:5])
+
+# print(s[-1] + s[-4] + "  " + s[-13])
+
+# list =[1,2,['hi','hello','bye'],99]
+
+# we want to print ello from nested list
+
+# print(list[2][1][1:4]) 
+# print(list[2][1][-4:-1]) 
+
+
+
+# String
+# String initialization
+# name="ashutosh"
+# print(name)
+
+# String conatenation
+# con=name+"bye"
+# print(con)
+
+name="ashutosh"
+
+# message=f"Hello {name} welcome to python"
+# print(message)
+
+# print(name[-8:-4:1])
+
+# print(name[::-1])
+
+# print(name*2)
+
+# print("ash" in name)
+
+
+
+
+
+
+
---
+++ list.py
@@ -0,0 +1,11 @@
+# List
+
+# List can be homogeneous and heterogeneous
+list1=[1,"ashut",[1,2,3],["hi","hello","bye"],99]
+# print(list1)
+
+list2=[1,2,3,4,5,6,7,8,9]
+
+# Mutablity in list
+list2[0]=100
+print(list2)
