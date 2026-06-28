5f9f7dce7c8bf56f11eba5ce1f2b3cfb82a9faaf
Ashutosh <ashut.sarkar8187@gmail.com>
2026-06-28 18:14:47 +0530

    completed insertion, creating, updation, serch and delete

--- list.py
+++ list.py
@@ -8,4 +8,43 @@ list2=[1,2,3,4,5,6,7,8,9]
 
 # Mutablity in list
 list2[0]=100
-print(list2)
+# print(list2)
+
+# empty list
+list3=[]
+# print(list3)
+
+# indexing and slicing in list
+# print(list2[0])
+# print(list2[1:4])
+
+# Reversing the list
+# print(list2[::-1])
+
+# inserting element at a particular index
+list2.insert(2,1000)
+# print(list2)
+
+# inserting value at the end of the list
+list2.append(2000)
+# print(list2)
+
+# inserting multiple values at the end of the list
+list2.extend([3000,4000,5000])
+# print(list2)
+
+# removing value from the list
+list2.remove(100)
+# print(list2)
+
+# removing value at a particular index
+removedValue= list2.pop(0)
+# print(removedValue)
+# print(list2)
+
+# list2.clear()
+# print(list2)
+
+# Getting length of list after removing all the elements from the list.
+# print(len(list2))
+
