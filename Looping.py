f1d93c7d80e2f83535b63789be61cf602c030818
Ashutosh <ashut.sarkar8187@gmail.com>
2026-07-11 13:05:40 +0530

    Done control statements questions using for loop

--- Looping.py
+++ Looping.py
@@ -42,15 +42,141 @@
 
 # print(sum)
 
-num=428
-while num>0:
-    last_digit=num%10
-    # print(last_digit)
-    num//=10
+# num=428
+# while num>0:
+#     last_digit=num%10
+#     # print(last_digit)
+#     num//=10
 
 
-reverse=str (num[::-1])
-print(reverse)
+# reverse=str (num[::-1])
+# print(reverse)
+
+# for var in 'hello':
+#     print(var)
+
+
+# s={'a':'list'}
+# for i in s.values():
+#     print(i)
+
+# s={'a':'list'}
+# for i in s.items():
+#     print(i)
+
+# s={'a':'list'}
+# for i in s:
+#     print(i)
+
+
+# for i in range(6):
+#     print(i)
+
+# for i in range(1,11,1):
+#     print(i)
+
+
+# for i in range(10, 0, -1):
+#     print(i)
+# n= eval(input("Enter the element of list--"))
+
+# lst= []
+
+# for i in range(1,n+1,1):
+#     lst.append(i)
+#     print(i)
+
+# print(lst)
+
+# n= int(input("Enter the no. of elements--"))
+
+# for i in range (n,0,-1):
+#     print(i)
+
+# n1=int(input("Enter n1--"))
+# n2=int(input("Enter n2--"))
+
+# for i in range(n1, n2+1,1):
+#     print(i)
+
+# n=int(input("Enter the number--"))
+
+# for i in range(1,11,1):
+#     print(n,"x",i,"=",n*i)
+
+# n=int(input("Enter the number--"))
+
+# for i in range(1,n+1,1):
+#     if(i&1==0):
+#       print(i)
+
+# n=int(input("Enter the number--"))
+
+# for i in range(1,n+1,1):
+#     if(i&1==1):
+#       print(i)
+
+# n=int(input("Enter the number--"))
+
+# for i in range(1,n+1,1):
+#     if(i%5==0):
+#       print(i)
+
+# n =int(input("Enter the number--"))
+
+# for i in range(1,n+1,1):
+#     if str(i)==str(i)[::-1]:
+#         print("The palindrome number is--",i)
+
+# n=int(input("Enter the number--")) 
+
+# sum=0
+
+# for i in range(1,n+1,1):
+#     sum=sum+i
+#     i+=1
+
+# print(sum)
+
+
+# n=int(input("Enter the number--")) 
+# sum=1
+# for i in range(1,n+1,1):
+#     sum=sum*i
+# print(sum)
+
+# n=int(input("Enter the number--"))
+# fact=1
+# for i in range(n,0,-1):
+#     fact=fact*i
+# print(fact)
+
+# n=int(input("Enter the number --"))
+# reverse=0
+# while(n!=0):
+#    digit= n%10
+#    reverse = reverse * 10 + digit
+#    n=n//10
+
+# print("Reversed number:", reverse)
+
+
+
+
+
+
+
+    
+
+
+  
+
+
+
+
+
+   
+
 
 
 
